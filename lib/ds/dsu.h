6fbd92d080250bb8e791247f76d7f3f12896754d
Zhaopeng Yan <yan-zp@foxmail.com>
2019-05-11 23:20:21 +0800

    dsu

---
+++ lib/ds/dsu.h
@@ -0,0 +1,24 @@
+
+
+struct Dsu {
+    int n;
+    vector<int> p;
+    
+    Dsu(int _n) { n = _n; p.resize(n); init(); }
+    inline void init() {
+        for (int i = 0; i < n; i++) {
+            p[i] = i;
+        }
+    }
+    inline int find(int x) {
+        return p[x] == x ? x: p[x] = find(p[x]);
+    }
+    inline bool join(int x, int y) {
+        x = find(x); y = find(y);
+        if (x == y) return false;
+        p[y] = x; return true;
+    }
+    inline bool check(int x, int y) {
+        return find(x) == find(y);
+    }
+};
