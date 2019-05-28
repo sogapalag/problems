5003f50a86a33091c0d31d273546dae1405329da
Zhaopeng Yan <yan-zp@foxmail.com>
2019-05-28 16:32:12 +0800

    add rmq sparse table in lib

---
+++ lib/ds/rmq.h
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// sparse table RMQ. insight, st[j][i], cover [i, i+(1<<j) )
+// unlike normal RMQ, use [j][i], not [i][j]. and store the index not actual element.
+// query [l, r)
+
+// SNIPPETS_START rmq
+template <typename T=int>
+struct Rmq {
+    vector<T> raw;
+    static const int LOG = 17; // 20;
+    int k;
+    int n;
+    vector<int> st[LOG];
+    Rmq(vector<T> _v) { // pass in vec[0, n)
+        raw = _v;
+        n = raw.size();
+        k = 32 - __builtin_clz(n);
+        for (int j = 0; j < k; j++) {
+            st[j].resize(n);
+        }
+        build();
+    }
+    inline int comp(int x, int y) { // DO! > max
+        return raw[x] < raw[y] ? x : y;
+    }
+    void build() {
+        for (int i = 0; i < n; i++) {
+            st[0][i] = i;
+        }
+        for (int j = 1; j < k; j++) {
+            for (int i = 0; i + (1<<j) <= n; i++) {
+                st[j][i] = comp(st[j-1][i], st[j-1][i + (1<<(j-1))]);
+            }
+        }
+    }
+    // [l, r)
+    inline int query_id(int l, int r) {
+        assert(0 <= l && l < r && r <= n);
+        int j = 31 - __builtin_clz(r - l);
+        return comp(st[j][l], st[j][r - (1<<j)]);
+    }
+    inline T query(int l, int r) {
+        return raw[query_id(l, r)];
+    }
+};
+// SNIPPETS_END
