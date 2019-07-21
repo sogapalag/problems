308bc6767eb39804536aa44c2e8e4b67d8d9d05f
Zhaopeng Yan <yan-zp@foxmail.com>
2019-07-21 17:23:25 +0800

    .

---
+++ codeforces/960e.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MOD = 1000000007; // 998244353
+
+template <typename T>
+struct modular {
+    constexpr modular() : val(0){}
+    constexpr modular(const modular<T>& _m) : val(_m.val) {}
+    template <typename U> constexpr modular(const U& _r = U()) {
+        val = -MOD <= _r && _r < MOD ? _r: _r % MOD;
+        if (val < 0) { val += MOD; } }
+    const T operator()() { return val; }
+    template <typename U> explicit operator U() const { return static_cast<U>(val); }
+    modular<T>& operator+=(const modular<T>& _m) { if ((val += _m.val) >= MOD) { val -= MOD; } return *this; }
+    modular<T>& operator-=(const modular<T>& _m) { if ((val -= _m.val) < 0) { val += MOD; } return *this; }
+    modular<T>& operator*=(const modular<T>& _m) { val = modular<T>(static_cast<int64_t>(val) * static_cast<int64_t>(_m.val)).val; return *this; }
+    modular<T>& operator/=(const modular<T>& _m) {
+        T a = _m.val, b = MOD, u = 0, v = 1;
+        while (a != 0) {
+            T q = b / a;
+            b -= q * a; swap(a, b);
+            u -= q * v; swap(u, v); 
+        } return *this *= u; }
+    modular<T>& operator =(const modular<T>& _m) { val = _m.val; return *this; }
+    template <typename U> modular<T>& operator+=(const U& _r) { return *this += modular<T>(_r); }
+    template <typename U> modular<T>& operator-=(const U& _r) { return *this -= modular<T>(_r); }
+    template <typename U> modular<T>& operator*=(const U& _r) { return *this *= modular<T>(_r); }
+    template <typename U> modular<T>& operator/=(const U& _r) { return *this /= modular<T>(_r); } 
+    template <typename U> modular<T>& operator =(const U& _r) { val = modular<T>(_r).val; return *this; }
+    modular<T> operator-() { return modular<T>(-val); }    
+    template <typename U> friend bool operator==(const modular<U>&, const modular<U>&);
+    friend std::istream& operator>>(std::istream& os, modular<T>& _m) { os >> _m.val; _m *= 1; return os; }
+    friend std::ostream& operator<<(std::ostream& os, const modular<T>& _m) { return os << _m.val; }
+    template <typename U>
+    modular<T> exp(U e) {
+        modular<T> res = 1;
+        modular<T> b = val;
+        if (e < 0) { b = 1/b; e *= -1; }
+        for (; e; e >>= 1) {
+            if (e & 1) { res *= b; }
+            b *= b;
+        } return res; }
+private:
+    T val;
+};
+template <typename T> inline modular<T> operator+(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) += _rhs; }
+template <typename T, typename U> inline modular<T> operator+(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) += _rhs; }
+template <typename T, typename U> inline modular<T> operator+(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) += _rhs; }
+template <typename T> inline modular<T> operator-(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) -= _rhs; }
+template <typename T, typename U> inline modular<T> operator-(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) -= _rhs; }
+template <typename T, typename U> inline modular<T> operator-(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) -= _rhs; }
+template <typename T> inline modular<T> operator*(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) *= _rhs; }
+template <typename T, typename U> inline modular<T> operator*(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) *= _rhs; }
+template <typename T, typename U> inline modular<T> operator*(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) *= _rhs; }
+template <typename T> inline modular<T> operator/(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) /= _rhs; }
+template <typename T, typename U> inline modular<T> operator/(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) /= _rhs; }
+template <typename T, typename U> inline modular<T> operator/(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) /= _rhs; }
+template <typename T> inline bool operator==(const modular<T>& _lhs, const modular<T>& _rhs) { return _lhs.val == _rhs.val; }
+template <typename T, typename U> inline bool operator==(const modular<T>& _lhs, const U& _rhs) { return _lhs == modular<T>(_rhs); }
+template <typename T, typename U> inline bool operator==(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) == _rhs; }
+template <typename T> inline bool operator!=(const modular<T>& _lhs, const modular<T>& _rhs) { return !(_lhs == _rhs); }
+template <typename T, typename U> inline bool operator!=(const modular<T>& _lhs, const U& _rhs) { return !(_lhs == _rhs); }
+template <typename T, typename U> inline bool operator!=(const U& _lhs, const modular<T>& _rhs) { return !(_lhs == _rhs); }
+typedef modular<int> mint;
+
+struct Node {
+    int sz[2];
+    mint mul;
+    bool d;
+    Node(): mul(0){}
+};
+
+// + iff start depth with u same pairity
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+    for (auto& x: a) {
+        cin >> x;
+    }
+    vector<Node> s(n);
+    vector<vector<int>> g(n);
+    for (int _ = 1; _ < n; _++) {
+        int x, y;
+        cin >> x >> y;
+        x--; y--;
+        g[x].emplace_back(y);
+        g[y].emplace_back(x);
+    }
+    vector<bool> trk(n, false);
+    function<void (int, bool)> dfs = [&](int u, bool d){//depth
+        s[u].d = d;
+        s[u].sz[d] = 1;
+        s[u].sz[d^1] = 0;
+        trk[u] = true;
+        for (int v: g[u]) {
+            if (!trk[v]) {
+                dfs(v, d^1);
+                mint diff = s[v].sz[d] - s[v].sz[d^1];
+                mint rem = n - s[v].sz[d] - s[v].sz[d^1];
+                s[u].mul += diff * rem;
+                s[u].sz[0] += s[v].sz[0];
+                s[u].sz[1] += s[v].sz[1];
+            }
+        }
+    };
+    dfs(0, 0);
+    mint res = 0;
+    for (int u = 0; u < n; u++) {
+        int d = s[u].d;
+        mint diff = s[0].sz[d] - s[u].sz[d] - s[0].sz[d^1] + s[u].sz[d^1];
+        mint rem = s[u].sz[0] + s[u].sz[1];
+        s[u].mul += diff * rem;
+        s[u].mul += n;
+
+        res += s[u].mul * a[u];
+    }
+    cout << res;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    cout << endl;
+}
