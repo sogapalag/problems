7902118a46475b7cbb0cb8bba0127130a5066608
Zhaopeng Yan <yan-zp@foxmail.com>
2019-08-07 20:06:13 +0800

    .

---
+++ codeforces/1197e.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MOD = 1000000007; // 998244353
+
+template <typename T>
+struct modular {
+    constexpr modular() : val(0){}
+    constexpr modular(const modular<T>& _m) : val(_m.val) {}
+    template <typename U> constexpr modular(const U& _r = U()) {
+        val = -MOD <= _r && _r < MOD ? _r: _r % MOD;
+        if (val < 0) { val += MOD; } }
+    const T operator()() { return val; }
+    template <typename U> explicit operator U() const { return static_cast<U>(val); }
+    modular<T>& operator+=(const modular<T>& _m) { if ((val += _m.val) >= MOD) { val -= MOD; } return *this; }
+    modular<T>& operator-=(const modular<T>& _m) { if ((val -= _m.val) < 0) { val += MOD; } return *this; }
+    modular<T>& operator*=(const modular<T>& _m) { val = modular<T>(static_cast<int64_t>(val) * static_cast<int64_t>(_m.val)).val; return *this; }
+    modular<T>& operator/=(const modular<T>& _m) {
+        T a = _m.val, b = MOD, u = 0, v = 1;
+        while (a != 0) {
+            T q = b / a;
+            b -= q * a; swap(a, b);
+            u -= q * v; swap(u, v); 
+        } return *this *= u; }
+    modular<T>& operator =(const modular<T>& _m) { val = _m.val; return *this; }
+    template <typename U> modular<T>& operator+=(const U& _r) { return *this += modular<T>(_r); }
+    template <typename U> modular<T>& operator-=(const U& _r) { return *this -= modular<T>(_r); }
+    template <typename U> modular<T>& operator*=(const U& _r) { return *this *= modular<T>(_r); }
+    template <typename U> modular<T>& operator/=(const U& _r) { return *this /= modular<T>(_r); } 
+    template <typename U> modular<T>& operator =(const U& _r) { val = modular<T>(_r).val; return *this; }
+    modular<T> operator-() { return modular<T>(-val); }    
+    template <typename U> friend bool operator==(const modular<U>&, const modular<U>&);
+    friend std::istream& operator>>(std::istream& os, modular<T>& _m) { os >> _m.val; _m *= 1; return os; }
+    friend std::ostream& operator<<(std::ostream& os, const modular<T>& _m) { return os << _m.val; }
+    template <typename U>
+    modular<T> exp(U e) {
+        modular<T> res = 1;
+        modular<T> b = val;
+        if (e < 0) { b = 1/b; e *= -1; }
+        for (; e; e >>= 1) {
+            if (e & 1) { res *= b; }
+            b *= b;
+        } return res; }
+private:
+    T val;
+};
+template <typename T> inline modular<T> operator+(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) += _rhs; }
+template <typename T, typename U> inline modular<T> operator+(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) += _rhs; }
+template <typename T, typename U> inline modular<T> operator+(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) += _rhs; }
+template <typename T> inline modular<T> operator-(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) -= _rhs; }
+template <typename T, typename U> inline modular<T> operator-(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) -= _rhs; }
+template <typename T, typename U> inline modular<T> operator-(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) -= _rhs; }
+template <typename T> inline modular<T> operator*(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) *= _rhs; }
+template <typename T, typename U> inline modular<T> operator*(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) *= _rhs; }
+template <typename T, typename U> inline modular<T> operator*(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) *= _rhs; }
+template <typename T> inline modular<T> operator/(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) /= _rhs; }
+template <typename T, typename U> inline modular<T> operator/(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) /= _rhs; }
+template <typename T, typename U> inline modular<T> operator/(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) /= _rhs; }
+template <typename T> inline bool operator==(const modular<T>& _lhs, const modular<T>& _rhs) { return _lhs.val == _rhs.val; }
+template <typename T, typename U> inline bool operator==(const modular<T>& _lhs, const U& _rhs) { return _lhs == modular<T>(_rhs); }
+template <typename T, typename U> inline bool operator==(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) == _rhs; }
+template <typename T> inline bool operator!=(const modular<T>& _lhs, const modular<T>& _rhs) { return !(_lhs == _rhs); }
+template <typename T, typename U> inline bool operator!=(const modular<T>& _lhs, const U& _rhs) { return !(_lhs == _rhs); }
+template <typename T, typename U> inline bool operator!=(const U& _lhs, const modular<T>& _rhs) { return !(_lhs == _rhs); }
+typedef modular<int> mint;
+
+struct Node {
+    int l, r;
+    Node() {}
+};
+struct State {
+    int cap;
+    mint num;
+    State() {}
+};
+// when range [l, r), has property notP~P, want first P.
+// when return r, means not found.
+template <typename T>
+T bs_first(T l, T r, function<bool (T)> f) {
+    assert(l < r);
+    T mid;
+    while (l != r) {
+        mid = l + (r-l)/2;
+        if (f(mid)) {
+            r = mid;
+        }else {
+            l = mid + 1;
+        }
+    }
+    return r;
+}
+void solve() {
+    int n; cin >> n;
+    vector<Node> a(n);
+    for (auto& p: a) {
+        cin >> p.r >> p.l;
+    }
+    // no need sort r when l=l case
+    sort(a.begin(), a.end(), [&](auto& u, auto& v){
+        return u.l < v.l;
+    });
+    vector<State> dp(n);
+    for (int i = n-1; i >= 0; i--) {
+        int pos = bs_first<int>(0, n, [&](int x){
+                return a[x].l >= a[i].r;
+                });
+        if (pos >= n) {
+            dp[i].cap = a[i].l;
+            dp[i].num = 1;
+        } else {
+            dp[i].cap = dp[pos].cap - (a[i].r - a[i].l);
+            dp[i].num = dp[pos].num;
+        }
+        if (i+1 >= n) continue;
+        if (dp[i].cap == dp[i+1].cap) {
+            dp[i].num += dp[i+1].num;
+        } else if (dp[i].cap > dp[i+1].cap) {
+            // ensure left-idx always best, then bs work
+            // note Node i may not in the set, dp[i] only track the best
+            dp[i].cap = dp[i+1].cap;
+            dp[i].num = dp[i+1].num;
+        }   
+    }
+    cout << dp[0].num;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    cout << endl;
+}
