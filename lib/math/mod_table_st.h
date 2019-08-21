62735d2777fbcaa3d508d6a379275a86e6cd8736
Zhaopeng Yan <yan-zp@foxmail.com>
2019-08-21 16:41:24 +0800

    upd mod_table

--- lib/math/mod_table_st.h
+++ lib/math/mod_table_st.h
@@ -3,42 +3,59 @@
 
 // SNIPPETS_START mod_table_st
 struct Comb {
-    int n;
+    int N;
     vector<mint> fac, ifac, inv;
 
     Comb(int _n=1) {
         resize(2);
         fac[0] = fac[1] = ifac[0] = ifac[1] = inv[1] = 1;
-        n = 1; grow(_n);
+        N = 1; grow(_n);
     }
 
     void resize(int _s) {
         fac.resize(_s); ifac.resize(_s); inv.resize(_s);
     }
     bool grow(int _n) {
-        if (_n <= n) return false;
-        resize(_n+1);
-        for (int i = n+1; i <= _n; i++) {
+        if (_n <= N) return false;
+        swap(_n, N); resize(N+1);
+        for (int i = _n+1; i <= N; i++) {
             fac[i] = fac[i-1] * i;
             inv[i] = (-MOD/i) * inv[MOD%i];
             ifac[i] = ifac[i-1] * inv[i];
         }
-        n = _n;
         return true;
     }
+    // perm
+    mint a(int n, int k) {
+        if (n < k || k < 0) return 0;
+        if (n > N) grow(n);
+        return fac[n] * ifac[n-k];
+    }
     // choose
-    mint c(int N, int K) {
-        if (N < K || K < 0) return 0;
-        if (N > n) grow(N);
-        return fac[N] * ifac[N-K] * ifac[K];
+    mint c(int n, int k) {
+        if (n < k || k < 0) return 0;
+        if (n > N) grow(n);
+        return fac[n] * ifac[n-k] * ifac[k];
     }
-    // sum x = S, x > 0
-    mint p(int S, int K) {
-        return c(S-1, K-1);
+    // sum x = s, x > 0
+    mint p(int s, int k) {
+        return c(s-1, k-1);
     }
-    // sum x = S, x >= 0
-    mint z(int S, int K) {
-        return p(S+K, K);
+    // sum x = s, x >= 0
+    mint z(int s, int k) {
+        return p(s+k, k);
+    }
+    // \E Y-X >= m
+    mint hit(int n, int k, int m=1) {
+        if (m <= k-n) return c(n+k, n);
+        return c(n+k, k-m);
+    }
+    // catalan trapezoids C_m(n, k)
+    // \A Y-X < m,  =c(n+k,n) - hit
+    mint cat(int n, int k, int m=1) {
+        if (m > k) return c(n+k, n);
+        if (m <= k-n) return 0;
+        return c(n+k,n) - c(n+k, k-m);
     }
 };
 // SNIPPETS_END
