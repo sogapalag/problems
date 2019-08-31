3c0e2cd31477009164dc86d7ec16484adb02f42f
Zhaopeng Yan <yan-zp@foxmail.com>
2019-08-31 20:47:12 +0800

    upd stir,sieve

---
+++ lib/math/sieve_st.h
@@ -0,0 +1,168 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// SNIPPETS_START sieve_st
+struct Sieve {
+    int sz;
+    vector<bool> is;
+    vector<int> prime;
+    Sieve(int n = 45000):sz(n) {
+        prime.reserve(n/6);
+        is.assign(n+1, true);
+        is[0] = is[1] = false;
+        for (int p = 2; (int64_t)p*p <= n; p++) {
+            if (is[p]) {
+                for (int i = p*p; i <= n; i += p) {
+                    is[i] = false;
+                }
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if (is[i])
+                prime.emplace_back(i);
+        }
+    }
+    // x=p0^e0 * p1^e1 *...
+    vector<pair<int, int>> factor(int x) {
+        assert(x > 0 && (int64_t)sz*sz >= x);
+        vector<pair<int, int>> res;
+        for (const int& p: prime) {
+            if ((int64_t)p*p > x) break;
+            if (x % p == 0) {
+                int e = 0;
+                while (x % p == 0) {
+                    e++; x /= p;
+                }
+                res.emplace_back(p, e);
+            }
+        }
+        if (x != 1)
+            res.emplace_back(x, 1);
+        return res;
+    }
+    // divisors
+    vector<int> divs(int x, bool sorted=false) {
+        vector<int> res;
+        res.emplace_back(1);
+        for (auto& p: factor(x)) {
+            int b, e;
+            tie(b, e) = p;
+            int sz = res.size();
+            int y = 1;
+            while (e--) {
+                y *= b;
+                for (int i = 0; i < sz; i++) {
+                    res.emplace_back(res[i] * y);
+                }
+            }
+        }
+        if (sorted) sort(res.begin(), res.end());
+        return res;
+    }
+    // number of divisors
+    int num_divs(int x) {
+        int res = 1;
+        for (auto& pe: factor(x)) {
+            res *= (1+pe.second);
+        }
+        return res;
+    }
+    // simple prime test
+    bool is_prime(int x) {
+        if (x <= sz) return is[x];
+        return factor(x)[0].first == x;
+    }
+    // prime divisors
+    vector<int> p_divs(int x) {
+        assert(x > 0 && (int64_t)sz*sz >= x);
+        vector<int> res;
+        for (const int& p: prime) {
+            if ((int64_t)p*p > x) break;
+            if (x % p == 0) {
+                while (x % p == 0) {
+                    x /= p;
+                }
+                res.emplace_back(p);
+            }
+        }
+        if (x != 1)
+            res.emplace_back(x);
+        return res;
+    }
+    // count [1..=m] coprime to c
+    int coprime(int m, int c){
+        int tmp = 0;
+        auto di = p_divs(c);
+        int sz = di.size();
+        for (int msk = 1; msk < (1<<sz); msk++) {
+            int prod = 1;
+            for (int k = 0; k < sz; k++) {
+                if (msk & (1<<k)) {
+                    prod *= di[k];
+                }
+            }
+            int sign = (__builtin_popcount(msk)&1)? 1: -1;
+            tmp += sign * m/prod;
+        }
+        return m - tmp;
+    };
+    // euler phi, [1..=x] coprime to x
+    int phi(int x) {
+        assert(x > 0);
+        if (x == 1) return 1;
+        int res = x;
+        for (auto& p: p_divs(x)) {
+            (res/=p)*=(p-1);
+        }
+        return res;
+    }
+    // pre-comp phi table
+    vector<int> phi_table(int n) {
+        assert(sz >= n);
+        vector<int> res(n+1);
+        iota(res.begin(), res.end(), 0);
+        for (const int& p: prime) {
+            if (p > n) break;
+            --res[p];
+            for (int i = 2*p; i <= n; i += p)
+                (res[i]/=p)*=p-1;
+        }
+        return res;
+    }
+    // mobius, (-1)^k; 0, p^2|x
+    int8_t mu(int x) {
+        assert(x > 0 && (int64_t)sz*sz >= x);
+        if (x == 1) return 1;
+        int cnt = 0;
+        for (const int& p: prime) {
+            if ((int64_t)p*p > x) break;
+            if (x % p == 0) {
+                cnt++; x /= p;
+            }
+            if (x % p == 0) {
+                return 0;
+            }
+        }
+        if (x != 1) cnt++;
+        return (cnt&1)?-1:1;
+    }
+    // pre-comp mobius table
+    vector<int8_t> mobius_table(int n) {
+        assert(sz >= n);
+        vector<int8_t> res(n+1, 1);
+        for (int i = 2; i <= n; i++) {
+            if (is[i]) res[i] = -1;
+            for (const int& p: prime) {
+                if (p > i || (int64_t)p*i > n) break;
+                if (i % p == 0) {
+                    res[i*p] = 0;
+                } else {
+                    res[i*p] = -res[i];
+                }
+            }
+        }
+        return res;
+    }
+};
+// SNIPPETS_END
---
+++ lib/math/table_stirling.h
@@ -0,0 +1,44 @@
+#include "modular_sim.h"
+
+// SNIPPETS_START table_stirling
+// [n k]: n into k cycles 
+vector<vector<mint>> stir_first(int n) {
+    vector<vector<mint>> stir(n+1, vector<mint>(n+1, 0));
+    stir[0][0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            stir[i][j] = stir[i-1][j-1] + stir[i-1][j]*(i-1);
+        }
+    }
+    return stir;
+}
+// {n k}: n into k sets
+vector<vector<mint>> stir_second(int n) {
+    vector<vector<mint>> stir(n+1, vector<mint>(n+1, 0));
+    stir[0][0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            stir[i][j] = stir[i-1][j-1] + stir[i-1][j]*j;
+        }
+    }
+    return stir;
+}
+// using, x^n = sum_{k:0..=n} {n k} (x)_k
+// (x)_n := x*(x-1)...(x-n+1)
+vector<mint> fall_table(int x, int n) {
+    vector<mint> res(n+1);
+    res[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        res[i] = res[i-1] * (x-i+1);
+    }
+    return res;
+}
+vector<mint> rise_table(int x, int n) {
+    vector<mint> res(n+1);
+    res[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        res[i] = res[i-1] * (x+i-1);
+    }
+    return res;
+}
+// SNIPPETS_END
