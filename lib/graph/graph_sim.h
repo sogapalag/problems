01369dc25ccc6caf57daf3d03a5cee409a483156
Zhaopeng Yan <yan-zp@foxmail.com>
2019-09-03 13:46:36 +0800

    .

--- lib/graph/graph_sim.h
+++ lib/graph/graph_sim.h
@@ -11,28 +11,46 @@ using namespace std;
 struct Graph {
     int n, m;
     vector<vector<int>> g;
+    bool is_digraph;
     Graph(int _n) : n(_n), m(0) {
         assert(n >= 1);
-        g.resize(n+1);
+        g.resize(n);
+        set_di(false);
     }
-    Graph(int _n, int _m) : n(_n), m(_m) {
+    Graph(int _n, int _m, bool _di=false) : n(_n), m(0) {
         assert(n >= 1);
         assert(1ll*n*(n-1)/2 >= m);
-        g.resize(n+1);
-        input();
+        g.resize(n);
+        set_di(_di);
+        input(_m);
     }
     inline void add(int u, int v) {
-        assert(1 <= u && u <= n);
-        assert(1 <= v && v <= n);
+        assert(0 <= u && u < n);
+        assert(0 <= v && v < n);
         assert(u != v);
         g[u].emplace_back(v);
-        g[v].emplace_back(u); // digraph remove
+        if (!is_digraph)
+            g[v].emplace_back(u);
+        m++;
     }
-    void input() {
+    void input(int _m) {
         int u, v;
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < _m; i++) {
             cin >> u >> v;
+            u--;v--; // to 0-based
             add(u, v);
+        }
+    }
+    void set_di(bool _di) {
+        is_digraph = _di;
+        trk = vector<bool>(n, false);
+    }
+    vector<bool> trk;
+    void dfs(int u) {
+        trk[u] = true;
+        for (int v: g[u]) {
+            if (trk[v]) continue;
+            //
         }
     }
 };
