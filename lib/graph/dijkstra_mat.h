7a49774df3220272fadc293641f68523778bfa41
Zhaopeng Yan <yan-zp@foxmail.com>
2019-09-14 19:40:37 +0800

    .

--- lib/graph/dijkstra_mat.h
+++ lib/graph/dijkstra_mat.h
@@ -2,60 +2,63 @@
 
 using namespace std;
 
-
-void solve(){
-
 // SNIPPETS_START graph_mat_dijkstra
-const int INF = 0x3f3f3f3f;
-const int N = 3000; 
+struct Graph {
+    int n;
+    const int INF = 0x3f3f3f3f;
+    vector<vector<int>> cost;
+    bool is_digraph;
 
-int cost[N][N];
-// move above global
-
-memset(cost, INF, sizeof cost);
-
-int n, m;
-cin >> n >> m;
-for (int i = 0; i < n; i++) {
-    cost[i][i] = 0;
-}
-struct Edge {
-    int u, v;
-    int c;
-};
-vector<Edge> e(m);
-for (int i = 0; i < m; i++) {
-    int x, y, z;
-    cin >> x >> y >> z;
-    x--; y--; // 1-based
-    cost[x][y] = cost[y][x] = min(cost[x][y], z);
-    e[i] = {x, y, z};
-}
-
-auto dijkstra = [&](int s){
-    vector<int> d(n, INF);
-    vector<bool> trk(n, false);
-    d[s] = 0;
-    while (true) {
-        int mindis = INF;
-        int u = -1;
+    Graph(int _n, bool _di=false) : n(_n) {
+        cost.resize(n);
         for (int i = 0; i < n; i++) {
-            if (!trk[i] && d[i] < mindis) {
-                u = i; mindis = d[i];
-            }
+            cost[i].assign(n, INF);
         }
-        if (!~u) break;
-        trk[u] = true;
-        for (int v = 0; v < n; v++) {
-            if (!trk[v] && d[v] > d[u] + cost[u][v]) {
-                d[v] = d[u] + cost[u][v];
+        for (int i = 0; i < n; i++) {
+            cost[i][i] = 0;
+        }
+        is_digraph = _di;
+        input();
+    }
+    void input() {
+        if (is_digraph) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    cin >> cost[i][j];
+                }
+            }
+        } else {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    int x; cin >> x;
+                    cost[i][j] = cost[j][i] = min(cost[i][j], x);
+                }
             }
         }
     }
-    return d;
+    template <typename T>
+    vector<T> dijkstra(int s){
+        //const T INF = 0x3f3f3f3f3f3f3f3f; 
+        vector<T> d(n, INF);
+        vector<bool> trk(n, false);
+        d[s] = 0;
+        while (true) {
+            T mindis = INF;
+            int u = -1;
+            for (int i = 0; i < n; i++) {
+                if (!trk[i] && d[i] < mindis) {
+                    u = i; mindis = d[i];
+                }
+            }
+            if (!~u) break;
+            trk[u] = true;
+            for (int v = 0; v < n; v++) {
+                if (!trk[v] && d[v] > d[u] + cost[u][v]) {
+                    d[v] = d[u] + cost[u][v];
+                }
+            }
+        }
+        return d;
+    }
 };
-
-auto d = dijkstra(0);
 // SNIPPETS_END
-
-}
