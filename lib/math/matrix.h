8e3c288f08ea5b26df2664b922d004a846e2e760
Zhaopeng Yan <yan-zp@foxmail.com>
2019-11-25 16:25:47 +0800

    add lib matrix

--- lib/math/matrix.h
+++ lib/math/matrix.h
@@ -1,38 +1,122 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-//~ SNIPPETS_START matrix
-namespace math {
-    // A = A X B, 0-based, n:dim
-    const int COL = 101;
-    void mat_mult(int n, mint A[][COL], mint B[][COL]){
-        mint res[n][n];
+
+
+// SNIPPETS_START matrix
+// n X n, for linear recurrence use
+template <typename T>
+struct Matrix {
+    int n;
+    vector<vector<T>> a;
+
+    // Caution: default I
+    constexpr Matrix(int _n) : n(_n) {
+        a.resize(n);
+        for (int i = 0; i < n; i++) {
+            a[i].resize(n);
+        }
+        set_id();
+    }
+    constexpr Matrix(const Matrix& _m) {
+        n = _m.n;
+        a = _m.a;
+    }
+    Matrix& operator=(const Matrix& _m) {
+        n = _m.n;
+        a = _m.a;
+        return *this;
+    }
+    void set_all(const T& val) {
+        for (auto& r: a) {
+            for (auto& x: r) {
+                x = val;
+            }
+        }
+    }
+    void set_id() {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                res[i][j] = 0;
+                a[i][j] = i==j? 1 : 0;
+            }
+        }
+    }
+    T trace() {
+        T sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += a[i][i];
+        }
+        return sum;
+    }
+    // sometimes use for v*A
+    void transpose() {
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                swap(a[i][j], a[j][i]);
+            }
+        }
+    }
+    
+    Matrix& operator+=(const Matrix& _m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] += _m.a[i][j];
+            }
+        }
+        return *this;
+    }
+    Matrix& operator-=(const Matrix& _m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] -= _m.a[i][j];
+            }
+        }
+        return *this;
+    }
+    // if impl as a = tmp*_m.a will WA when A*=A;
+    Matrix& operator*=(const Matrix& _m) {
+        auto r = a;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                r[i][j] = 0;
                 for (int k = 0; k < n; k++) {
-                    res[i][j] += A[i][k] * B[k][j];
+                    r[i][j] += a[i][k] * _m.a[k][j];
                 }
             }
         }
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                A[i][j] = res[i][j];
+        a = r;
+        return *this;
     }
-    
-    // A = A^exp, n:dim
-    void mat_pow(int n, mint A[][COL], long long exp){
-        mint res[n][COL];
-        memset(res, 0, sizeof res);
-        for (int i = 0; i < n; i++)
-            res[i][i] = 1;
-        for(; exp; exp >>= 1){
-            if (exp & 1)
-                mat_mult(n, res, A);
-            mat_mult(n, A, A);
+    // r = A*v
+    vector<T> operator*(const vector<T>& v) {
+        assert(n == (int)v.size());
+        vector<T> res(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                res[i] += a[i][j] * v[j];
+            }
         }
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                A[i][j] = res[i][j];
+        return res;
     }
-}
-//~ SNIPPETS_END
+    Matrix operator+(const Matrix& _m) {
+        return Matrix(*this) += _m;
+    }
+    Matrix operator-(const Matrix& _m) {
+        return Matrix(*this) -= _m;
+    }
+    Matrix operator*(const Matrix& _m) {
+        return Matrix(*this) *= _m;
+    }
+    template <typename U>
+    Matrix exp(U e) {
+        assert(e >= 0);
+        Matrix res(n);
+        Matrix b = *this;
+        for (; e; e >>= 1) {
+            if (e&1) res *= b;
+            b *= b;
+        }
+        return res;
+    }
+};
+// SNIPPETS_END
