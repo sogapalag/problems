db9690dc6da5915c49a6a20b2a615de3535e7882
Zhaopeng Yan <yan-zp@foxmail.com>
2019-12-30 22:47:31 +0800

    .

---
+++ codeforces/1086e.cpp
@@ -0,0 +1,185 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MOD = 998244353; // 998244853;
+
+template <typename T>
+struct modular {
+    constexpr modular() : val(0){}
+    constexpr modular(const modular<T>& _m) : val(_m.val) {}
+    template <typename U> constexpr modular(const U& _r = U()) {
+        val = -MOD <= _r && _r < MOD ? _r: _r % MOD;
+        if (val < 0) { val += MOD; } }
+    const T operator()() { return val; }
+    template <typename U> explicit operator U() const { return static_cast<U>(val); }
+    modular<T>& operator+=(const modular<T>& _m) { if ((val += _m.val) >= MOD) { val -= MOD; } return *this; }
+    modular<T>& operator-=(const modular<T>& _m) { if ((val -= _m.val) < 0) { val += MOD; } return *this; }
+    modular<T>& operator*=(const modular<T>& _m) { val = modular<T>(static_cast<int64_t>(val) * static_cast<int64_t>(_m.val)).val; return *this; }
+    modular<T>& operator/=(const modular<T>& _m) {
+        T a = _m.val, b = MOD, u = 0, v = 1;
+        while (a != 0) {
+            T q = b / a;
+            b -= q * a; swap(a, b);
+            u -= q * v; swap(u, v); 
+        } return *this *= u; }
+    modular<T>& operator =(const modular<T>& _m) { val = _m.val; return *this; }
+    template <typename U> modular<T>& operator+=(const U& _r) { return *this += modular<T>(_r); }
+    template <typename U> modular<T>& operator-=(const U& _r) { return *this -= modular<T>(_r); }
+    template <typename U> modular<T>& operator*=(const U& _r) { return *this *= modular<T>(_r); }
+    template <typename U> modular<T>& operator/=(const U& _r) { return *this /= modular<T>(_r); } 
+    template <typename U> modular<T>& operator =(const U& _r) { val = modular<T>(_r).val; return *this; }
+    modular<T> operator-() { return modular<T>(-val); }    
+    template <typename U> friend bool operator==(const modular<U>&, const modular<U>&);
+    friend std::istream& operator>>(std::istream& os, modular<T>& _m) { os >> _m.val; _m *= 1; return os; }
+    friend std::ostream& operator<<(std::ostream& os, const modular<T>& _m) { return os << _m.val; }
+    template <typename U>
+    modular<T> exp(U e) {
+        modular<T> res = 1;
+        modular<T> b = val;
+        if (e < 0) { b = 1/b; e *= -1; }
+        for (; e; e >>= 1) {
+            if (e & 1) { res *= b; }
+            b *= b;
+        } return res; }
+private:
+    T val;
+};
+template <typename T> inline modular<T> operator+(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) += _rhs; }
+template <typename T, typename U> inline modular<T> operator+(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) += _rhs; }
+template <typename T, typename U> inline modular<T> operator+(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) += _rhs; }
+template <typename T> inline modular<T> operator-(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) -= _rhs; }
+template <typename T, typename U> inline modular<T> operator-(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) -= _rhs; }
+template <typename T, typename U> inline modular<T> operator-(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) -= _rhs; }
+template <typename T> inline modular<T> operator*(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) *= _rhs; }
+template <typename T, typename U> inline modular<T> operator*(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) *= _rhs; }
+template <typename T, typename U> inline modular<T> operator*(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) *= _rhs; }
+template <typename T> inline modular<T> operator/(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) /= _rhs; }
+template <typename T, typename U> inline modular<T> operator/(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) /= _rhs; }
+template <typename T, typename U> inline modular<T> operator/(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) /= _rhs; }
+template <typename T> inline bool operator==(const modular<T>& _lhs, const modular<T>& _rhs) { return _lhs.val == _rhs.val; }
+template <typename T, typename U> inline bool operator==(const modular<T>& _lhs, const U& _rhs) { return _lhs == modular<T>(_rhs); }
+template <typename T, typename U> inline bool operator==(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) == _rhs; }
+template <typename T> inline bool operator!=(const modular<T>& _lhs, const modular<T>& _rhs) { return !(_lhs == _rhs); }
+template <typename T, typename U> inline bool operator!=(const modular<T>& _lhs, const U& _rhs) { return !(_lhs == _rhs); }
+template <typename T, typename U> inline bool operator!=(const U& _lhs, const modular<T>& _rhs) { return !(_lhs == _rhs); }
+typedef modular<int> mint;
+
+template <typename T=int>
+struct Fenwick {
+    int n;
+    vector<T> v;
+    
+    Fenwick(int size = 100005) : n(size), v(n+1, 0) {}
+    inline void add(int p, T val) {
+        for (; p <= n; p += (p&-p)) { v[p] += val; }
+    }
+    inline T query(int p) {
+        T tmp = 0;
+        for (; p > 0; p -= (p&-p)) { tmp += v[p]; }
+        return tmp;
+    }
+    inline T query(int l, int r) {
+        return query(r) - query(l-1);
+    }
+};
+#undef LOCAL
+string to_string(string s) { return '"' + s + '"'; }
+string to_string(bool x) { return (x ? "T" : "F"); }
+string to_string(const char* s) { return to_string((string)s); }
+template <typename A, typename B>
+string to_string(pair<A, B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename V>
+string to_string(V v) {
+    bool f = true; string res = "[";
+    for (const auto& x: v) {
+        if (!f) res += ", ";
+        f = false;
+        res += to_string(x); } res += "]";
+    return res;
+}
+void debug() { cerr << endl; }
+template <typename H, typename... T>
+void debug(H h, T... t) { cerr << " " << to_string(h); debug(t...); }
+#ifdef LOCAL
+#define dbg(args...) cerr<<"("<<#args<<") =", debug(args)
+#else
+#define dbg(args...) 2019
+#endif
+
+const int N = 2048; 
+mint dp[N][N];// generalized derangement, i,j : perm[i] with j constraint, j de. pi!=u
+mint pw[N];
+int a[N][N];
+
+// it's trivial that total n! (!n)^{n-1} ways.
+void solve() {
+    int n; cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> a[i][j];
+        }
+    }
+    // prep
+    dp[0][0] = 1;
+    for (int i = 1; i <= n; i++) {
+        dp[i][0] = dp[i-1][0] * i; // n!
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            dp[i][j] = dp[i][j-1] - dp[i-1][j-1];
+        }
+    }
+    pw[0] = 1; pw[1] = dp[n][n]; // !n
+    for (int i = 2; i <= n; i++) {
+        pw[i] = pw[i-1] * pw[1];
+    }
+
+    mint res = 0;
+    {// first row is kth of n!
+        Fenwick<int> f(n);
+        for (int j = 1; j <= n; j++) {
+            int x = a[1][j];
+            int les = (x-1) - f.query(x);
+            res += les * dp[n-j][0];
+            f.add(x, 1);
+        }
+        res *= pw[n-1];
+    }
+    // for each row, with prev-row as de. conditions
+    for (int i = 2; i <= n; i++) {
+        mint tmp = 0;
+        Fenwick<int> de(n), f(n);
+        vector<bool> vis(n+1), trk(n+1);
+        int cnt = n;
+        for (int j = 1; j <= n; j++) {
+            int u = a[i-1][j];
+            int x = a[i][j];
+            int les = (x-1) - f.query(x) - (u<x && !trk[u]);
+            int seen = de.query(x-1); // those de occur already
+            // note there're two cases, either
+            //     y    or  y
+            //   y             y 
+            // cnt is trigged by first y, thus !other[y]
+            // de is trigged by second shape, +,-.
+            if (!trk[u]) de.add(u, 1), cnt--;
+dbg(les, seen, cnt);
+            tmp += seen * dp[n-j][cnt];
+            tmp += (les-seen) * dp[n-j][cnt-1]; // !trk[y], like below, thus -1
+            // ocuur twice, deactive
+            if (vis[x]) de.add(x, -1); else cnt--;
+            vis[u] = true; trk[x] = true;
+            f.add(x, 1);
+dbg(cnt);
+        }
+        res += tmp * pw[n-i];
+    }
+    cout << res;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    cout << endl;
+}
