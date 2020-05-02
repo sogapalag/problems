25acb5076e0aaca4628e0e1cb94a9d8f4ddc1fdb
Zhaopeng Yan <yan-zp@foxmail.com>
2020-05-02 16:28:00 +0800

    .

--- codeforces/1348e.cpp
+++ codeforces/1348e.cpp
@@ -39,9 +39,45 @@ void solve() {
     cout << res;
 }
 
+// note we can actually care dp[r] true/false, i.e. can use #red for one r+b
+// since although two (r+b), we can treat it as one (r+b) (note, not meanning it's valid, but ajust valid combi. is fine) 
+// thus we can treat find any (r+b) valid, remain can treat as same group, since there must be some valid corresponding.
+// key optim: dp[i] contribute to a range i+[l..r], optim by delta.
+void fast() {
+    int n,m;
+    cin >> n >> m;
+    vector<bool> dp(m);
+    vector<int> cnt(2*m);
+    ll sx = 0, sy = 0;
+    dp[0] = true;
+    for (int _ = 0; _ < n; _++) {
+        int x,y;
+        cin >> x >> y;
+        sx += x; sy += y;
+        fill(cnt.begin(), cnt.end(), 0);
+        for (int i = 0; i < m; i++)if(dp[i]) {
+            int l = max(1, m-y);
+            int r = min(m-1, x);
+            if (l <= r) {
+                cnt[i+l]++; cnt[i+r+1]--;
+            }
+        }
+        for (int i = 1; i < 2*m; i++) {
+            cnt[i] += cnt[i-1];
+            if (cnt[i] > 0) dp[i%m] = true;
+        }
+    }
+    ll res = 0;
+    for (int i = 0; i < m; i++)if(dp[i]) {
+        res = max(res, (i>0) + (sx-i)/m + (sy-(m-i)%m)/m );
+    }
+    cout << res;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    solve();
+    fast();
+    //solve();
     return 0;
 }
