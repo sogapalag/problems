92caa47f9143bd557d919c62e6eeea21ec351ba0
Zhaopeng Yan <yan-zp@foxmail.com>
2020-05-21 22:37:56 +0800

    .

---
+++ oi/noi/2018/p4769.cpp
@@ -0,0 +1,177 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MOD = 998244353; // 998244853; 
+
+template <typename T>
+struct modular {
+    constexpr modular() : val(0){}
+    constexpr modular(const modular<T>& _m) : val(_m.val) {}
+    template <typename U> constexpr modular(const U& _r = U()) {
+        val = -MOD <= _r && _r < MOD ? _r: _r % MOD;
+        if (val < 0) { val += MOD; } }
+    const T operator()() { return val; }
+    template <typename U> explicit operator U() const { return static_cast<U>(val); }
+    modular<T>& operator+=(const modular<T>& _m) { if ((val += _m.val) >= MOD) { val -= MOD; } return *this; }
+    modular<T>& operator-=(const modular<T>& _m) { if ((val -= _m.val) < 0) { val += MOD; } return *this; }
+    modular<T>& operator*=(const modular<T>& _m) { val = modular<T>(static_cast<int64_t>(val) * static_cast<int64_t>(_m.val)).val; return *this; }
+    modular<T>& operator/=(const modular<T>& _m) {
+        T a = _m.val, b = MOD, u = 0, v = 1;
+        while (a != 0) {
+            T q = b / a;
+            b -= q * a; swap(a, b);
+            u -= q * v; swap(u, v); 
+        } return *this *= u; }
+    modular<T>& operator =(const modular<T>& _m) { val = _m.val; return *this; }
+    template <typename U> modular<T>& operator+=(const U& _r) { return *this += modular<T>(_r); }
+    template <typename U> modular<T>& operator-=(const U& _r) { return *this -= modular<T>(_r); }
+    template <typename U> modular<T>& operator*=(const U& _r) { return *this *= modular<T>(_r); }
+    template <typename U> modular<T>& operator/=(const U& _r) { return *this /= modular<T>(_r); } 
+    template <typename U> modular<T>& operator =(const U& _r) { val = modular<T>(_r).val; return *this; }
+    modular<T> operator-() { return modular<T>(-val); }    
+    template <typename U> friend bool operator==(const modular<U>&, const modular<U>&);
+    friend std::istream& operator>>(std::istream& os, modular<T>& _m) { os >> _m.val; _m *= 1; return os; }
+    friend std::ostream& operator<<(std::ostream& os, const modular<T>& _m) { return os << _m.val; }
+    template <typename U>
+    modular<T> exp(U e) {
+        modular<T> res = 1;
+        modular<T> b = val;
+        if (e < 0) { b = 1/b; e *= -1; }
+        for (; e; e >>= 1) {
+            if (e & 1) { res *= b; }
+            b *= b;
+        } return res; }
+private:
+    T val;
+};
+template <typename T> inline modular<T> operator+(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) += _rhs; }
+template <typename T, typename U> inline modular<T> operator+(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) += _rhs; }
+template <typename T, typename U> inline modular<T> operator+(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) += _rhs; }
+template <typename T> inline modular<T> operator-(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) -= _rhs; }
+template <typename T, typename U> inline modular<T> operator-(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) -= _rhs; }
+template <typename T, typename U> inline modular<T> operator-(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) -= _rhs; }
+template <typename T> inline modular<T> operator*(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) *= _rhs; }
+template <typename T, typename U> inline modular<T> operator*(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) *= _rhs; }
+template <typename T, typename U> inline modular<T> operator*(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) *= _rhs; }
+template <typename T> inline modular<T> operator/(const modular<T>& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) /= _rhs; }
+template <typename T, typename U> inline modular<T> operator/(const modular<T>& _lhs, const U& _rhs) { return modular<T>(_lhs) /= _rhs; }
+template <typename T, typename U> inline modular<T> operator/(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) /= _rhs; }
+template <typename T> inline bool operator==(const modular<T>& _lhs, const modular<T>& _rhs) { return _lhs.val == _rhs.val; }
+template <typename T, typename U> inline bool operator==(const modular<T>& _lhs, const U& _rhs) { return _lhs == modular<T>(_rhs); }
+template <typename T, typename U> inline bool operator==(const U& _lhs, const modular<T>& _rhs) { return modular<T>(_lhs) == _rhs; }
+template <typename T> inline bool operator!=(const modular<T>& _lhs, const modular<T>& _rhs) { return !(_lhs == _rhs); }
+template <typename T, typename U> inline bool operator!=(const modular<T>& _lhs, const U& _rhs) { return !(_lhs == _rhs); }
+template <typename T, typename U> inline bool operator!=(const U& _lhs, const modular<T>& _rhs) { return !(_lhs == _rhs); }
+typedef modular<int> mint;
+
+struct Comb {
+    int N;
+    vector<mint> fac, ifac, inv;
+
+    Comb(int _n=1) {
+        resize(2);
+        fac[0] = fac[1] = ifac[0] = ifac[1] = inv[1] = 1;
+        N = 1; grow(_n);
+    }
+
+    void resize(int _s) {
+        fac.resize(_s); ifac.resize(_s); inv.resize(_s);
+    }
+    bool grow(int _n) {
+        if (_n <= N) return false;
+        swap(_n, N); resize(N+1);
+        for (int i = _n+1; i <= N; i++) {
+            fac[i] = fac[i-1] * i;
+            inv[i] = (-MOD/i) * inv[MOD%i];
+            ifac[i] = ifac[i-1] * inv[i];
+        }
+        return true;
+    }
+    // perm
+    mint a(int n, int k) {
+        if (n < k || k < 0) return 0;
+        if (n > N) grow(n);
+        return fac[n] * ifac[n-k];
+    }
+    // choose
+    mint c(int n, int k) {
+        if (n < k || k < 0) return 0;
+        if (n > N) grow(n);
+        return fac[n] * ifac[n-k] * ifac[k];
+    }
+    // sum x = s, x > 0
+    mint p(int s, int k) {
+        if (s==0&&k==0)return 1;
+        return c(s-1, k-1);
+    }
+    // sum x = s, x >= 0
+    mint z(int s, int k) {
+        return p(s+k, k);
+    }
+    // \E Y-X >= m
+    mint hit(int n, int k, int m=1) {
+        if (m <= k-n) return c(n+k, n);
+        return c(n+k, k-m);
+    }
+    // catalan trapezoids C_m(n, k)
+    // \A Y-X < m,  =c(n+k,n) - hit
+    mint cat(int n, int k, int m=1) {
+        if (m > k) return c(n+k, n);
+        if (m <= k-n) return 0;
+        return c(n+k,n) - c(n+k, k-m);
+    }
+}c(2e6);
+
+// first note the valid form: two group {x>i}/or not, both inc.
+// let's consider no constraint, how to produce one from 1,2..,n
+//  for x=1..n, each time either stay, or insert it(to be group {x>i}) before, but should after(mx=max{x>i})
+//  then by consider dp[i][j], till i, mx_pos=j,
+// recall https://en.wikipedia.org/wiki/Catalan%27s_triangle
+// one can notice res = Cat(n,n)
+// now consider if we fix some prefix.
+//   one can see only needed info are i and mx=max(x>i)=max(prefix)
+//   i.e. for remain nn=#free could insert., m=#less
+//   [..]_m , [..]_nn
+//   m = 0, Cat(nn,nn), one can easily guess, Cat_m+1(nn, nn+m)
+//     which is correct, since one can notice first row(of cat'tri.) are, (m+1) 1s.
+// more detail:
+//   for iter first i s.t. y=pi>x (lexi condition)
+//   we can just consider the case y in insert-group, (otherwise contradiction, since mi<=x<y must in other group)
+//   thus y = (z..n]. we don't need iter y. just by IEP, we minus the case mi as first.
+void solve() {
+    int n; cin >> n;
+    vector<bool> trk(n + 1);
+    mint res = 0;
+    bool fine = true;
+    // a: max(x>i)
+    // b: max(x<=i)
+    // mi: unused min
+    for (int i = 1, a = 0, b = 0, mi = 1; i <= n; i++) {
+        int x; cin >> x;
+        int z = max({i,x,a});
+        int nn = n-z;
+        int len = n-i+1;
+        if (fine && nn > 0 && b < mi) res += c.cat(nn, len, len-nn+1) - c.cat(nn, len-1, len-nn);
+        if (x > i) {
+            if (a < x) a = x;
+            else fine = false;
+        } else {
+            if (b < x) b = x;
+            else fine = false;
+        }
+        trk[x] = true;
+        while (trk[mi]) ++mi;
+    }
+    cout << res << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T; cin >> T;
+    for (int t = 0; t < T; t++) {
+        solve();
+    }
+    return 0;
+}
